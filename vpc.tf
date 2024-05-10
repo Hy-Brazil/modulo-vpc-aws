297a2238cd8ddf6481a965ce3ca7e208dde69285
romuloff22 <romuloffranco@gmail.com>
2024-05-10 16:51:38 -0300

    add dns hosts2

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "vpc-homo" {
   cidr_block = var.cidr_vpc
-  enable_dns_hostnames = "enable"
+  enable_dns_hostnames = true
 
   tags = {
     Name       = "vpc-${var.environment}"
