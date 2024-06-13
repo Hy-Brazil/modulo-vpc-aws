099749d2643da8af7fd9c693999eea11e36d486e
Rômulo Franco <romuloffranco@gmail.com>
2024-06-13 11:22:07 -0300

    removendo o nat gateway

--- route_tables.tf
+++ route_tables.tf
@@ -13,22 +13,6 @@ resource "aws_route_table" "route_table" {
   }
 }
 
-resource "aws_route_table" "route_table2" {
-  vpc_id = aws_vpc.vpc-homo.id
-
-  route {
-    cidr_block = var.route_table_nat_gw
-    gateway_id = aws_nat_gateway.gw_nat.id
-  }
-
-  tags = {
-    Name       = "route-table2-${var.environment}"
-    owner      = "romulo"
-    managed-by = "terraform"
-  }
-}
-
-
 resource "aws_route_table_association" "rta1" {
   subnet_id      = aws_subnet.subnet01.id
   route_table_id = aws_route_table.route_table.id
@@ -42,9 +26,4 @@ resource "aws_route_table_association" "rta2" {
 resource "aws_route_table_association" "rta3" {
   subnet_id      = aws_subnet.subnet03.id
   route_table_id = aws_route_table.route_table.id
-}
-
-resource "aws_route_table_association" "rta4" {
-  subnet_id      = aws_subnet.subnet04.id
-  route_table_id = aws_route_table.route_table2.id
 }
