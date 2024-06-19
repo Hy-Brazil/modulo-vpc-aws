713849ade55597ff694a8939f44d61af1226ae7e
Rômulo Franco <romuloffranco@gmail.com>
2024-06-19 10:13:50 -0300

    alterando security group 3

--- security_groups.tf
+++ security_groups.tf
@@ -210,23 +210,22 @@ resource "aws_security_group" "security_group_02" {
 
 resource "aws_security_group" "seurity_group_03" {
   name        = "launch-wizard-3-${var.environment}"
-  description = "Permitir acesso a porta 6379"
+  description = "Portas liberadas na instancia Jenkins"
   vpc_id      = aws_vpc.vpc-homo.id
 
   ingress {
-    description = "SSL"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    description = "all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["10.0.0.0/16"]
   }
 
-  ingress {
-    description = "banco-redis"
-    from_port   = 6379
-    to_port     = 6379
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
