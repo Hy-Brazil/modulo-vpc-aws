6040c976508ce6f9efeb8cc34c125c1299cc5bff
Rômulo Franco <romuloffranco@gmail.com>
2024-06-19 10:16:44 -0300

    alterando porta security group 3

--- security_groups.tf
+++ security_groups.tf
@@ -214,12 +214,20 @@ resource "aws_security_group" "seurity_group_03" {
   vpc_id      = aws_vpc.vpc-homo.id
 
   ingress {
-    description = "all"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "SSL"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
 
   egress {
     from_port   = 0
